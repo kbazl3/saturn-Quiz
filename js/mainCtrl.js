c35d581c1b187fba0b60dad3cdbbaba75da6fdce
Kyle Barlow <kbazlo@yahoo.com>
2017-06-11 21:37:47 -0600

    this has to work

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,7 +6,7 @@ angular.module('saturnQuiz')
         $scope.activeQuestionAnswer = 0;
         $scope.percentage = 0;
 
-        $http.get('/../quiz_data.json').then(function(quizData) {
+        $http.get('./../quiz_data.json').then(function(quizData) {
             $scope.myQuestions = quizData.data;
             $scope.totalQuestions = $scope.myQuestions.length;
         });
